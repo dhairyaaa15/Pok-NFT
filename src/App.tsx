95dbedebe2162b74b232fbfcc735fb9ba444b1a4
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-09-06 11:39:34 +0530

    Loading-effect for main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Explore from "./pages/Explore"
 import './index.css';
+import Main from "./pages/Main";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
 
